62411ff0551fff042ebfdd7b151b16923c57dd67
Mahdi Samavi <samavi.mahdi@gmail.com>
2025-06-11 09:16:24 +0330

    chore: add migrate to auto migration when not exists tables

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import QueryClientProvider from './providers/query-client-provider';
 import RouterWithContextProvider from './providers/router-with-context-provider';
+import migrate from './db/migrate';
 
 import './styles/global.css';
+
+migrate();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
